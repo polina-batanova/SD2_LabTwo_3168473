4bf040367952106cb85023c8539ac2a89cab987b
polinabatanova <polab98@gmail.com>
2026-02-22 10:21:54 +0000

    Added method stubs to Conversions class

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,4 +1,22 @@
 package griffith;
 
 public class Conversions {
+
+    public double euroToDollar(double euro) {
+        return 0;
+    }
+    public double dollarToEuro(double dollar) {
+        return 0;
+    }
+
+    public int stringToInteger(String val) {
+        return 0;
+    }
+    public String integerToString(int val) {
+        return null;
+    }
+
+    public String switchCase(String input) {
+        return null;
+    }
 }
