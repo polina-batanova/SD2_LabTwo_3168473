526fcf00a2e2e4aa780e4d0a142861ece3406e4f
polinabatanova <polab98@gmail.com>
2026-02-22 11:07:07 +0000

    Implemented stringToInteger method

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -14,7 +14,7 @@ public class Conversions {
     }
 
     public int stringToInteger(String val) {
-        return 0;
+        return Integer.parseInt(val);
     }
     public String integerToString(int val) {
         return null;
